394675751623b290c155d9f7c816b20be9038633
KronosGR <geo.elgeo@gmail.com>
2023-01-18 10:35:30 +0100

    Revert "stage 4/4 completed"
    This reverts commit 019f2ca42483bf9d4d7dd32ea4776ec1ad0a560c.

--- Car Sharing/task/src/carsharing/CarDao.java
+++ Car Sharing/task/src/carsharing/CarDao.java
@@ -9,16 +9,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-public class CarDao implements Dao {
+public class CarDao implements Dao{
 
     static Connection connection = H2.getConnection();
 
     static String Q_SELECT_ALL = "select * from car";
     static String Q_SELECT_ALL_BY_COMPANY = Q_SELECT_ALL + " where company_id=?";
-    static String Q_SELECT_ALL_BY_NAME = Q_SELECT_ALL + " where name=?";
-    static String Q_SELECT_ALL_BY_ID = Q_SELECT_ALL + " where id=?";
     static String Q_CREATE = "insert into car (name, company_id) values(?,?)";
 
     @Override
@@ -28,63 +25,31 @@ public class CarDao implements Dao {
 
     @Override
     public boolean create(Object obj) {
-        Car tmp = (Car) obj;
-        try (PreparedStatement preparedStatement = connection.prepareStatement(Q_CREATE)) {
+        Car tmp = (Car)obj;
+        try(PreparedStatement preparedStatement= connection.prepareStatement(Q_CREATE)) {
             preparedStatement.setString(1, tmp.getName());
             preparedStatement.setInt(2, tmp.getCompanyId());
             preparedStatement.execute();
-        } catch (SQLException e) {
+        } catch (SQLException e){
             System.out.println("Error while inserting");
             return false;
         }
         return true;
     }
 
-    @Override
-    public boolean update(Object obj) {
-        // no need for this one
-        return false;
-    }
-
-    @Override
-    public boolean delete(Object obj) {
-        // no need for this one
-        return false;
-    }
-
-    @Override
-    public Optional getByName(String name) {
-        Optional<Car> car = Optional.empty();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(Q_SELECT_ALL_BY_NAME)) {
-            preparedStatement.setString(1, name);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                car = Optional.of(new Car(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name"),
-                        resultSet.getInt("company_id")
-                ));
-            }
-        } catch (SQLException e) {
-            System.out.println("Error while getting car");
-        }
-        return car;
-    }
-
-    public List<Car> getCarsByCompany(Company company) {
+    public List<Car> getCarsByCompany(Company company){
         List<Car> cars = new ArrayList<>();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(Q_SELECT_ALL_BY_COMPANY)) {
+        try(PreparedStatement preparedStatement = connection.prepareStatement(Q_SELECT_ALL_BY_COMPANY)){
             preparedStatement.setInt(1, company.getId());
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                cars.add(new Car(resultSet.getInt("id"),
+            while (resultSet.next()){
+                cars.add(new Car (resultSet.getInt("id"),
                         resultSet.getString("name"),
                         resultSet.getInt("company_id")
-                ));
+                        ));
             }
-        } catch (SQLException e) {
+        } catch (SQLException e){
             System.out.println("Error while getting the company's cars");
             return null;
         }
